0eb460b260ff9a70b981278ee3b2ae873e58d6c7
Johnny Taylor <johnny@abledaccess.com>
2014-12-08 15:03:38 -0500

    MASTER: Add tag context helpers
    More information is provided to help users understand the words
    appearing to the left of post tags are in fact tags which the
    post has been filed with. Simply accomplished with the addition
    of the words 'post tagged'.

--- access/abled-content/themes/abled-twentyfourteen/content.php
+++ access/abled-content/themes/abled-twentyfourteen/content.php
@@ -62,5 +62,5 @@
 	</div><!-- .entry-content -->
 	<?php endif; ?>
 
-	<?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
+	<?php the_tags( '<footer class="entry-meta">Post tagged <span class="tag-links">', '', '</span></footer>' ); ?>
 </article><!-- #post-## -->
