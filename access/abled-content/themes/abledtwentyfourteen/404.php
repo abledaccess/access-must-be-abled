348e58d9a4a84b1f6f396b235dd2adb266429c13
Johnny Taylor <johnny@abledaccess.com>
2015-11-14 11:06:50 -0500

    MASTER: Update 404.php copy

--- access/abled-content/themes/abledtwentyfourteen/404.php
+++ access/abled-content/themes/abledtwentyfourteen/404.php
@@ -13,15 +13,18 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 
-			<header class="none-header">
-				<h1 class="none-title"><?php _e( 'Not Found', 'abledtwentyfourteen' ); ?></h1>
+			<header class="page-header">
+				<h1 class="page-title"><?php _e( 'Page Not Found', 'abledtwentyfourteen' ); ?></h1>
 			</header>
 
-			<div class="page-content">
-				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search next?', 'abledtwentyfourteen' ); ?></p>
+			<section class="page-content">
+				<header class="none-header">
+					<h2 class="none-title">That could have gone much better...</h2>
+				</header>
+				<p><?php _e( 'Well, unfortunately, it looks as though what you were after isn\'t here. Maybe try a search below?', 'abledtwentyfourteen' ); ?></p>
 
 				<?php get_search_form(); ?>
-			</div><!-- .page-content -->
+			</section><!-- .page-content -->
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
