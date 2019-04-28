9ce33a17f0af79de1ff2693b2439c10051a6e2db
Johnny Taylor <johnny@abledaccess.com>
2019-04-28 13:49:39 -0400

    Add favicon and touch-icon

--- wp-content/themes/abledtwentyfourteen/header.php
+++ wp-content/themes/abledtwentyfourteen/header.php
@@ -24,6 +24,8 @@
 	<title><?php wp_title( '&ndash;', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="shortcut icon" type="image/png" href="/images/icon/favicon.png" />
+	<link rel="shortcut icon" type="image/apple-touch-icon" href="/images/icon/touch-icon.png" /> 
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
